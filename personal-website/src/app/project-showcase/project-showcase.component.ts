4764bcc4eb5072c79bae2afb910b8878bf5541d0
Aidan-Ingram <aidan.ingram03@gmail.com>
2024-11-04 14:16:42 -0600

    Added components to project space

---
+++ personal-website/src/app/project-showcase/project-showcase.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-showcase',
+  standalone: true,
+  imports: [],
+  templateUrl: './project-showcase.component.html',
+  styleUrl: './project-showcase.component.css'
+})
+export class ProjectShowcaseComponent {
+
+}
